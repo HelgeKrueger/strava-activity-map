3e4628122b7524ba583c6cdebdf3c7f40da0a205
Helge <helge.kruegr@gmail.com>
2016-02-21 17:50:20 +0100

    Changing default tile server

--- frontend.js
+++ frontend.js
@@ -10,7 +10,8 @@ var lineOptions = {
     opacity: 0.7,
     lineJoin: 'round'
 };
-var tiles = 'http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg';
+//var tiles = 'http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg';
+var tiles = 'http://a.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var layer = L.tileLayer(tiles, {maxZoom: 18});
 var map = L.map('map').setView([51.505, -0.09], 13);
 layer.addTo(map);
